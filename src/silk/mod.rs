ad98d3e029451987f515996824034e0902d263dd
cijiugechu <cijiugechu@gmail.com>
2025-11-21 22:08:49 +0800

    port noise shape analysis

--- src/silk/mod.rs
+++ src/silk/mod.rs
@@ -74,6 +74,7 @@ pub mod nlsf_stabilize;
 pub mod nlsf_unpack;
 pub mod nlsf_vq;
 pub mod nlsf_vq_weights_laroia;
+pub mod noise_shape_analysis;
 pub mod pitch_analysis_core;
 pub mod pitch_est_tables;
 pub mod plc;
